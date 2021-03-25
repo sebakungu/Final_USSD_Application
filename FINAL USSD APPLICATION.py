156c955520644bcba844c524f46fa0bfeaf226c5
sebakungu <76170152+sebakungu@users.noreply.github.com>
2021-03-25 17:33:43 +0200

    Add files via upload

---
+++ FINAL USSD APPLICATION.py
@@ -0,0 +1,260 @@
+def langauges():
+    print("Welcome to Kiziba Refugee camp USSD application")
+    print("1. English")
+    print("2. Kinyarwanda")
+    print("3. Kiswahili")
+    print("4. French")
+def wrong_selection(message):
+    print(message)
+
+def patners():
+    print("Welcome to Kiziba Refugee Camp Patners")
+    print("1. PAM")
+    print("2. AHA")
+    print("3. MINEMA")
+    print("4. WORLD VISION")
+    print("5. ARC")
+    print("6. PLAN")
+    print("7. PFR")
+    print("0. Back")
+def patners_kinyarwanda():
+    print("Murakaza neza kumurongo mumenyeraho amakuru yabafatanya bikorwa munkambi ya kiziba")
+    print("1. PAM")
+    print("2. AHA")
+    print("3. MINEMA")
+    print("4. WORLD VISION")
+    print("5. ARC")
+    print("6. PLAN")
+    print("7. PFR")
+    print("0. Back")
+def patners_Swahili():
+    print("Karibu Kwa Marafiki wa mtandaoni katika kambi ya Kiziba")
+    print("1. PAM")
+    print("2. AHA")
+    print("3. MINEMA")
+    print("4. WORLD VISION")
+    print("5. ARC")
+    print("6. PLAN")
+    print("7. PFR")
+    print("0. Back")
+def patners_French():
+    print("Binvenue dans la base de donnees en ligne des parties prenantes du camp de Kiziba")
+    print("1. PAM")
+    print("2. AHA")
+    print("3. MINEMA")
+    print("4. WORLD VISION")
+    print("5. ARC")
+    print("6. PLAN")
+    print("7. PFR")
+    print("0. Back")
+
+def PAM():
+    print("Welcome to Kiziba PAM")
+    print("1. Money distribution updates")
+    print("2. MVISA issue")
+    print("0. Back")
+def PAM_kinyarwanda():
+    print("Murakaza neza kwishami ryahashiseri rishizwe tuganga ibiribwa kumunzi PAM")
+    print("1. Amakuru yogutanga amafaranga munkambi ya kiziba")
+    print("2. Ikibazo cya MVISA")
+    print("0. Gusubira inyuma")
+def PAM_French():
+    print("Les mises à jour de la distribution d'argent seront bientôt mises à jour")
+def PAM_Swahili():
+    print("Sasisho za usambazaji wa pesa zitasasishwa hivi karibuni")
+
+def Money_distribution():
+    print("Welcome to PAM for money distribution schedule")
+    print("1. Money for food schdule")
+    print("2. Money for equipment schdule")
+    print("3. Pad and soap distribution")
+    print("4. Money for fuel")
+    print("5. Back")
+def Food_Kinyarwanda():
+    print("Murakaza kureba ingegabiye yo kwifurakwiza amafaranga nubundi bufasha PAM iha impunzi munkambi ya kiziba")
+    print("1. Ingengabihe yo gutanga amafaraga")
+    print("2. Ingengabihe yo gutanga ibikoresho")
+    print("3. Ingengabihe yo gutanga amasabune na pad kugitsina dore")
+    print("4. Ingengabihe yo gutanga amafaranga yibicanwa")
+    print("5. Gusubira inyuma")
+def Food():
+    print("PAM is informing all kiziba community that money for food will be distributed start from 10th to 18th, April 2021 ")
+def Food_Kinyarwanda():
+    print("PAM iramenyesha abatuye munkambi ya kiziba ko amafaranga bibyo kurya akatagwa kari 10 kugeza 18 ukukwezi")
+def Equipment():
+    print("PAM is informing you all that money for equipment will distributed on 17th, June 2021. So, please share this schudle to concerned people you may know")
+def Equipment_Kinyarwanda():
+    print("Amafaranga yibikoresho aza tagwa tariki 17 kugeza tariki 18 ukwakira")
+def Pad():
+    print("PAM is impressed to let all kiziba community member that pads and soop will be distributed on 15th, May 2021")
+def Pad_Kinyarwanda():
+    print("PAM izabamenyesha igihe cyo gutanga PAD kubategarugori")
+def Fuel():
+    print("PAM is informing Kiziba Community that money for fuel will be distrubited on 20th, June 2021")
+def Fuel_Kinyarwanda():
+    print("PAM irabamenyesha ko izatanga amafaranga yikwi tariki 20 ukuboza.")
+def MINEMA():
+    print("UNHCR and MINEMA is impressed to invite all Kiziba members for general meeting which will take place at Baskball ground")
+def MINEMA_Swahili():
+    print("UNHCR na MINEMA wamevutiwa kuwaalika washiriki wote wa Kiziba kwa mkutano mkuu utakaofanyika kwenye uwanja wa Baskball")
+
+def AHA():
+    print("Kiziba hostipal is informing all Kiziba community to keeping applying  Covid-19 measures and preservation because ten more people have  tested positive this Friday")
+def AHA_kinya():
+    print("Umuyobozi bwititaro byo munkambi ya Kiziba buranyeshako kwambara agapfukamunwa, koga intoki keshi ndetse no gusiga intera yametero ko arubumwe muburyo bwo kwirinda icyorezo cya Covid-19")
+def AHA_Swahili():
+    print("Jeshi la Kiziba linajulisha jamii yote ya Kiziba kuendelea kutumia hatua na uhifadhi wa Covid-19 kwa sababu watu wengine kumi wamepatikana na Ijumaa")
+def AHA_French():
+    print("Jeshi la Kiziba linajulisha jamii yote ya Kiziba kuendelea kutumia hatua na uhifadhi wa Covid-19 kwa sababu watu wengine kumi wamepatikana na Ijumaa")
+def WVR():
+    print("DAFI is empressed to let all secondary graduate that it is providing scholarship for best performers. Whoever is interest can vist World Vision office for furthemore information")
+def WVR_French():
+    print("DAFI est obligé de laisser tous les diplômés du secondaire qu'il offre des bourses aux meilleurs interprètes. Quiconque est intéressé peut visiter le bureau de Vision Mondiale pour plus d'informations")
+def WVR_Kinyarwanda():
+    print("World Vision iramenyesha abanyeshuri bose bifuza kukomera amashuri yisumbuye ko DAFI yafunguye imiryango. Abashaka kwiga basura ibiro bya World Vision kumekuru arambuye")
+def MINEMA():
+    print("MINEMA is impressed to let all kiziba community member that whoever lost his ID he may come to MINEMA office, starting from Monday, for help")
+def MVISA_issue():
+    print("Kiziba PAM is informing people WITH MMVISA issue that it is welcoming  thier questions starting from 4th, May 2021. Please share this new to those it may concern")
+def MVISA_isssue_kinyarwanda():
+    print("PAM iramenyesha abantu bose bafite ibibazo birebana nudukarita ko basura ibiro bya PAM guhera kuruyu wambare mumasaha yo gukora")
+def ARC():
+    print("ARC at Kiziba is informing all Kiziba community that it is providing wood to those who registered for repiaring thier house.")
+def ARC_Kinyarwanda():
+    print("ARC iramenyesha abantu bose biyandikishije gusanirwa amazu ko izatangira gubanga ibiti guhera kuwambere mucyumweru gitaha")
+def PLAN():
+    print("Plan is impressed to invite all Kiziba community to attend International children day,  and the event will take place at Basketball ground")
+def Plan_Kinyarwanda():
+    print("Plan itumiye abantu bose tuzaza kwizihiza umunsi mukuru mpuzamahanga wabana kukibuga cya Basket. Ntihazahire ubura cyangwa ngo akererwe. ")
+def PFR():
+    print("Prison Fellowship Rwanda is impressed to invite all Kiziba Community to attend law conginition workshop, and the event will take place at Barcelona hall. Whowever is interested can visit our office for further information")
+def PFR_Kinyarwanda():
+    print("PFR itumiye abantu bose mumahugurwa agamiye kusobanura amategeko. Amahurwa azatangira kuruyu wa kane. Ntihagire ubura cyangwa ngo akererwe")
+langauges()
+input1 = int(input())
+if input1 == 1:
+    patners()
+    input11 = int(input())
+    if input11 == 1:
+        PAM()
+    elif input11 == 2:
+        AHA()
+    elif input11 ==3:
+        MINEMA()
+    elif input11 == 4:
+         WVR()
+    elif input11 ==5:
+        ARC()
+    elif input11 ==6:
+        PLAN()
+    elif input11 ==7:
+        PFR()
+    elif input11 ==0:
+        langauges()
+    else:
+        wrong_selection("Unkown Partners selection. Please try again later")
+        patners()
+    input99 = int(input())
+    if input99 == 1:
+        Money_distribution()
+    elif input99 == 2:
+        MVISA_issue()
+    elif input99 ==0:
+        patners()
+    else:
+        wrong_selection("Unkown PAM selection. Please try again later")
+        PAM()
+
+    input13 = int(input())
+    if input13 ==1:
+        Food()
+    elif input13 ==2:
+        AHA_kinya()
+    elif input13 ==2:
+        Equipment()
+    elif input13 ==3:
+        Pad()
+    elif input13 ==4:
+        Fuel()
+    elif input13 ==0:
+        PAM()
+    else:
+        wrong_selection("Unkown money distribution schedule selection. Please try again later")
+        Money_distribution()
+elif input1 ==2:
+    patners_kinyarwanda()
+    input50 = int(input())
+    if input50 ==1:
+        PAM_kinyarwanda()
+    elif input50 ==2:
+        AHA_kinya()
+    elif input50 ==3:
+        WVR_Kinyarwanda()
+    elif input50 ==4:
+        WVR_Kinyarwanda()
+    elif input50 ==5:
+        ARC_Kinyarwanda()
+    elif input50 ==6:
+        Plan_Kinyarwanda()
+    elif input50 ==7:
+        PFR_Kinyarwanda()
+    elif input50 ==0:
+        langauges()
+    else:
+        wrong_selection("Umuhisemo umubare utariwe. Mwongere mukanya")
+        patners_kinyarwanda()
+
+    input78 = int(input())
+    if input78 ==1:
+        Food_Kinyarwanda()
+    elif input78 ==2:
+        MVISA_isssue_kinyarwanda()
+    elif input78 ==0:
+        PAM_kinyarwanda()
+    elif input78 ==2:
+        Equipment_Kinyarwanda()
+    elif input78 ==3:
+        Pad_Kinyarwanda()
+    elif input78 ==4:
+        Fuel_Kinyarwanda()
+    elif input78 ==0:
+        PAM_kinyarwanda()
+       
+
+    else:
+        wrong_selection("Muhisemo umubare utazwi. Mbonengere mugerageze")
+        PAM_kinyarwanda()
+elif input1 ==3:
+    patners_Swahili()
+    input60 = int(input())
+    if input60 ==1:
+        PAM_Swahili()
+    elif input ==2:
+        AHA_Swahili()
+    elif input ==3:
+        MINEMA_Swahili()
+elif input1 ==4:
+    patners_French()
+    input70 =int(input())
+    if input70 ==1:
+        PAM_French()
+    elif input70 ==4:
+        WVR_French()
+
+else:
+    wrong_selection("Unknown Language selection. Please try again later")
+    langauges()
+    
+
+
+
+    
+
+
+    
+    
+
+
+
+
+
